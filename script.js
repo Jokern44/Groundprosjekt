67d1c461c1d77d31e63664bd1a5ab0a49a8cef55
Jokern44 <06leilia@stud.akademiet.no>
2026-03-10 14:21:37 +0100

    mer fixes

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { createClient } from "@supabase/supabase-js";
-
 const supabaseUrl = "https://uputgeqzixncefxjzdkz.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVwdXRnZXF6aXhuY2VmeGp6ZGt6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNDQwNTYsImV4cCI6MjA4ODcyMDA1Nn0.d_N3g5r7ZbKkQhXFPf3e3LgCOY1ommZSGdILwgGZy7E"; // kun lese-tilgang
